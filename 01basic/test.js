0fb4fcc10cbde7cb58a1953d9cf00aff4e07eb04
Yashh129 <155818380+Yashh129@users.noreply.github.com>
2024-01-08 08:34:17 +0000

    test

--- 01basic/test.js
+++ 01basic/test.js
@@ -1,1 +1,10 @@
-console.log("Rahul")
+let score= "33"
+let y=Number(score)
+let x=String(y)
+let z=null
+let k=Number(z)
+console.log(typeof(z));
+console.log(typeof(k));
+console.log(k);
+node test
+
