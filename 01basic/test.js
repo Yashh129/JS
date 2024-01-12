e424d70d4f989894dd76ba49b4cfc36c5a504ef6
Yashh129 <155818380+Yashh129@users.noreply.github.com>
2024-01-12 14:26:34 +0000

    test.js

--- 01basic/test.js
+++ 01basic/test.js
@@ -1,10 +1,51 @@
-let score= "33"
-let y=Number(score)
-let x=String(y)
-let z=null
-let k=Number(z)
-console.log(typeof(z));
-console.log(typeof(k));
-console.log(k);
-node test
+function sum(...num1){
+    let tsum=0;
+  for(let i of num1){
+    tsum+=i;
+  }
+  return tsum;
+}
+// console.log(sum(200,300,400));
+
+const user ={
+    username:"Rahul",
+    price:199
+}
+
+function handleobject({username:a,price}){
+    return `usernmae is ${a} and price is ${price}`
+}
+// console.log(handleobject(user));
+
+// function fun1(){
+   
+//     function fun2(){
+//         console.log(a);
+//         console.log(b);
+//     }
+//     let b=20;
+//     fun2()
+// }
+
+// fun1()
+// const fun1=  function() {
+//     console.log(this);
+// }
+// fun1()
+// const fun=  () => {
+//     console.log(this);
+// }
+// fun()
+
+(() =>{
+      console.log("Hello");
+})();
+
+((name) =>{
+    console.log("Hello " +name );
+})("Rahul")
+
+
+
+
 
